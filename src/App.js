bc9c8e9e3e16eef01ba2e8ba0951436d80173983
Bahng Ji Hoon <wpxm2003@naver.com>
2019-12-13 23:24:07 +0900

    뉴스페이지 만들기 기초완료

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import axios from 'axios';
+import NewsList from './components/NewsList';
 
-function App() {
+const App = () => {
+  //const [data, setData] = useState(null);
+  // const onClick = () => {
+  //   axios.get('https://jsonplaceholder.typicode.com/todos/1').then(Response => {
+  //     setData(Response.data);
+  //   });
+  // };
+  // const onClick = async () => {
+  //   try {
+  //     const Response = await axios.get(
+  //       'https://newsapi.org/v2/top-headlines?country=kr&apiKey=ef0edae4cf0b4ae999520df2bb177e3b',
+  //     );
+  //     setData(Response.data);
+  //   } catch (e) {
+  //     console.log(e);
+  //   }
+  // };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <NewsList />
+      {/* <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )} */}
     </div>
   );
-}
+};
 
 export default App;
