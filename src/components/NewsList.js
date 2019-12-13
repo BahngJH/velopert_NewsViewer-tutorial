6563bc7db374e42e02d1972912a8aa690cc4f585
Bahng Ji Hoon <wpxm2003@naver.com>
2019-12-14 00:04:40 +0900

    api로 뉴스 불러오는거까지 성공

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import NewsItem from './NewsItem';
+import axios from 'axios';
 
 const NewsListBlock = styled.div`
   box-sizing: border-box;
@@ -21,17 +22,47 @@ const sampleArticle = {
   url: 'https://google.com',
   urlToImage: 'https://via.placeholder.com/160',
 };
+
 const NewsList = () => {
+  const [articles, setArticles] = useState(null);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    //async를 사용하는 함수 따로 선언
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(
+          'https://newsapi.org/v2/top-headlines?country=kr&apiKey=ef0edae4cf0b4ae999520df2bb177e3b',
+        );
+        setArticles(response.data.articles);
+      } catch (e) {
+        console.log(e);
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return <NewsListBlock>대기중...</NewsListBlock>;
+  }
+  if (!articles) {
+    return null;
+  }
   return (
     <div>
       <NewsListBlock>
+        {articles.map(article => (
+          <NewsItem key={articles.url} article={article} />
+        ))}
+
+        {/* <NewsItem article={sampleArticle} />
         <NewsItem article={sampleArticle} />
         <NewsItem article={sampleArticle} />
         <NewsItem article={sampleArticle} />
         <NewsItem article={sampleArticle} />
         <NewsItem article={sampleArticle} />
-        <NewsItem article={sampleArticle} />
-        <NewsItem article={sampleArticle} />
+        <NewsItem article={sampleArticle} /> */}
       </NewsListBlock>
     </div>
   );
